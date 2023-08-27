d7a3a93451ea306c88668f7372c66492ef3bb2da
daniel ben avi <ben.avi.daniel@s.afeka.ac.il>
2023-08-27 18:49:21 +0300

    update command controller in main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from flask_restful import reqparse, abort, Api, Resource
+from REST.command_controller import CommandController
 from REST.user_controller import UserControllerReadUpdate, UserControllerCreate
 from REST.object_controller import ObjectControllerReadUpdate, ObjectContollerCreate
 from flask_pymongo import PyMongo
@@ -16,5 +17,8 @@ api.add_resource(UserControllerCreate, '/wastewize/users')
 # Object Routes
 api.add_resource(ObjectControllerReadUpdate, '/wastewize/objects/<object_id>')
 api.add_resource(ObjectContollerCreate, '/wastewize/objects')
+
+# Command Routes
+api.add_resource(CommandController, '/wastewize/commands')
 
 app.run()
