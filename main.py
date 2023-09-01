4fd11c0755bb5232cd7aeb259e93d5aa098da0da
DeanAvram <dean6198@gmail.com>
2023-09-01 15:52:43 +0300

    Change all occurrence of WasteWize to Wastewise

--- main.py
+++ main.py
@@ -5,30 +5,32 @@ from REST.user_controller import UserControllerReadUpdate, UserControllerCreate
 from REST.object_controller import ObjectControllerReadUpdate, ObjectContollerCreate
 from REST.admin_controller import AdminControllerUsers, AdminControllerObjects, AdminControllerCommands
 
+
 def create_app():
     app = Flask(__name__)
     app.config['ENV'] ='development'
     return app
 
-application = app = create_app()
-api = Api(app)
+
+application = create_app()
+api = Api(application)
 # User Routes
-api.add_resource(UserControllerReadUpdate, '/wastewize/users/<user_email>')
-api.add_resource(UserControllerCreate, '/wastewize/users')
+api.add_resource(UserControllerReadUpdate, '/wastewise/users/<user_email>')
+api.add_resource(UserControllerCreate, '/wastewise/users')
 
 # Object Routes
-api.add_resource(ObjectControllerReadUpdate, '/wastewize/objects/<object_id>')
-api.add_resource(ObjectContollerCreate, '/wastewize/objects')
+api.add_resource(ObjectControllerReadUpdate, '/wastewise/objects/<object_id>')
+api.add_resource(ObjectContollerCreate, '/wastewise/objects')
 
 # Command Routes
-api.add_resource(CommandController, '/wastewize/commands')
+api.add_resource(CommandController, '/wastewise/commands')
 
 # Admin Routes
-api.add_resource(AdminControllerUsers, '/wastewize/admin/users')
-api.add_resource(AdminControllerObjects, '/wastewize/admin/objects')
-api.add_resource(AdminControllerCommands, '/wastewize/admin/commands')
+api.add_resource(AdminControllerUsers, '/wastewise/admin/users')
+api.add_resource(AdminControllerObjects, '/wastewise/admin/objects')
+api.add_resource(AdminControllerCommands, '/wastewise/admin/commands')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
 
 
--- service/main_service.py
+++ service/main_service.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 class MainService:
     def __init__(self):
         self.client = MongoClient('localhost', 27017)
-        self.db = self.client.wastewize
+        self.db = self.client.wastewise
 
     def get_db(self):
         return self.db
