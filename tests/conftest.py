579cf1653dbd86bddecdfbd2889fd5fd98835204
Daniel Ben-Avi <daniel.ben.avi10@gmail.com>
2023-09-03 19:11:56 +0300

    add data driven testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,14 @@
+from pathlib import Path
 import pytest
+import json
+import os
 
 from src import create_app
+from src.services.object_service import ObjectService
+
+
+objectService = ObjectService()
+
 
 @pytest.fixture
 def app():
@@ -18,4 +26,18 @@ def client(app):
     return app.test_client()
 
 
+@pytest.fixture(autouse=True)
+def before_and_after_test():
+    # clear database before each test
+    objectService.db.objects.delete_many({}) 
 
+    yield
+
+def get_test_data(filename) -> dict:
+    folder_path = os.path.abspath(Path(os.path.dirname(__file__)))
+    folder = os.path.join(folder_path, 'test_data')
+    jsonfile = os.path.join(folder, filename)
+    with open(jsonfile) as file:
+        data = json.load(file)
+
+    return data
