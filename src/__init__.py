a57117a4e4249d203e10fc928c1532586843d27f
Daniel Ben-Avi <daniel.ben.avi10@gmail.com>
2023-09-04 23:17:46 +0300

    reformat files

--- src/__init__.py
+++ src/__init__.py
@@ -7,17 +7,17 @@ from src.controller.command_controller import commands
 from src.controller.admin_controller import admin
 from src.controller.predict_controller import predict
 
+
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    
+
     if test_config is None:
         app.config.from_mapping(
             SEKRET_KEY=os.environ.get('SECRET_KEY'),
         )
     else:
         app.config.from_mapping(test_config)
-    
-    
+
     app.register_blueprint(objects)
     app.register_blueprint(users)
     app.register_blueprint(commands)
--- src/data/command.py
+++ src/data/command.py
@@ -3,14 +3,14 @@ from time import time
 import uuid
 import json
 
+
 @dataclass
-class Command():
-    
+class Command:
     _id: str
     type: str
     invoked_by: str
     data: dict
-    created_at: int 
+    created_at: int
 
     def __init__(self, type: str, invoked_by: str):
         self._id = str(uuid.uuid1())
@@ -20,29 +20,29 @@ class Command():
 
     def get_id(self):
         return self._id
-    
+
     def get_type(self):
         return self.type
 
     def get_invoked_by(self):
         return self.invoked_by
-    
+
     def get_data(self):
         return self.data
-    
+
     def get_created_at(self):
         return self.created_at
-    
+
     def set_data(self, data: dict):
         self.data = data
 
     def __str__(self) -> str:
         return f'{self._id} {self.type} {self.invoked_by} {self.data} {self.created_at}'
-    
+
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__,
                           sort_keys=True, indent=4)
-    
+
     def fromJSON(self, json_object: dict):
         self._id = json_object['_id']
         self.type = json_object['type']
