4437c5fd08d9bf521d7af9de901e9b382a3e776a
DeanAvram <dean6198@gmail.com>
2023-11-03 18:49:13 +0200

    Added password validation to admin service

--- src/controller/admin_controller.py
+++ src/controller/admin_controller.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from flask import request
 from src.services.rest.admin_service import AdminService
 from flask import abort, make_response, jsonify
+from src.controller.main_controller import MainController
 
 
 admin = Blueprint('admin', __name__, url_prefix='/wastewise/admin')
@@ -11,32 +12,34 @@ adminService = AdminService()
 
 @admin.get('/users')
 def get_all_users():
-    return adminService.get_all_users(_get_page(request), _get_limit(request),_get_user_email(request))
+    return adminService.get_all_users(_get_page(request), _get_limit(request),
+                                      MainController.get_user_email(request), MainController.get_user_password(request))
 
 
 @admin.delete('/users')
 def delete_users():
-    return adminService.delete_users(_get_user_email(request))
+    return adminService.delete_users(MainController.get_user_email(request), MainController.get_user_password(request))
 
 
 @admin.get('/objects')
 def get_all_objects():
-    return adminService.get_all_objects(_get_page(request), _get_limit(request), _get_user_email(request))
+    return adminService.get_all_objects(_get_page(request), _get_limit(request),
+                                        MainController.get_user_email(request), MainController.get_user_password(request))
 
 
 @admin.delete('/objects')
 def delete_objects():
-    return adminService.delete_objects(_get_user_email(request))
+    return adminService.delete_objects(MainController.get_user_email(request), MainController.get_user_password(request))
 
 
 @admin.get('/commands')
 def get_all_commands():
-    return adminService.get_all_commands(_get_page(request), _get_limit(request), _get_user_email(request))
+    return adminService.get_all_commands(_get_page(request), _get_limit(request), MainController.get_user_email(request), MainController.get_user_password(request))
 
 
 @admin.delete('/commands')
 def delete_commands():
-    return adminService.delete_commands(_get_user_email(request))
+    return adminService.delete_commands(MainController.get_user_email(request), MainController.get_user_password(request))
 
 
 def _get_limit(req):
@@ -55,12 +58,3 @@ def _get_page(req):
     else:
         page = int(req.args.get('page'))
     return page
-
-
-def _get_user_email(req):
-    # get query param user_id
-    if req.args.get('email') is None:
-        abort(make_response(jsonify(message="Email is missing"), 400))
-    else:
-        user_id = req.args.get('email')
-    return user_id
--- src/services/rest/admin_service.py
+++ src/services/rest/admin_service.py
@@ -17,39 +17,32 @@ class AdminService(MainService):
         self.objects = super().get_db().objects
         self.commands = super().get_db().commands
 
-    def delete_users(self, email: str) -> tuple:
-        if not super().check_permissions(EnumRole.ADMIN, email):
-            return {"Error": "User doesn't have permissions"}, HTTPStatus.UNAUTHORIZED
-
+    def delete_users(self, email: str, password: str) -> tuple:
+        super().check_permissions(EnumRole.ADMIN, email, password)
         self.users.delete_many({})
         return '', HTTPStatus.NO_CONTENT
 
-    def get_all_users(self, page: int, limit: int, email: str) -> tuple:
-        if not super().check_permissions(EnumRole.ADMIN, email):
-            return {"Error": "User doesn't have permissions"}, HTTPStatus.UNAUTHORIZED
+    def get_all_users(self, page: int, limit: int, email: str, password: str) -> tuple:
+        super().check_permissions(EnumRole.ADMIN, email, password)
         to_skip = calc_to_skip(page, limit)
         return list(self.users.find({}).sort("name", ASCENDING).skip(to_skip).limit(limit)), HTTPStatus.OK
 
-    def delete_objects(self, email: str) -> tuple:
-        if not super().check_permissions(EnumRole.ADMIN, email):
-            return {"Error": "User doesn't have permissions"}, HTTPStatus.UNAUTHORIZED
+    def delete_objects(self, email: str, password: str) -> tuple:
+        super().check_permissions(EnumRole.ADMIN, email, password)
         self.objects.delete_many({})
         return '', HTTPStatus.NO_CONTENT
 
-    def get_all_objects(self, page: int, limit: int, email: str) -> tuple:
-        if not super().check_permissions(EnumRole.ADMIN, email):
-            return {"Error": "User doesn't have permissions"}, HTTPStatus.UNAUTHORIZED
+    def get_all_objects(self, page: int, limit: int, email: str, password: str) -> tuple:
+        super().check_permissions(EnumRole.ADMIN, email, password)
         to_skip = calc_to_skip(page, limit)
         return list(self.objects.find({}).sort("type", ASCENDING).skip(to_skip).limit(limit)), HTTPStatus.OK
 
-    def delete_commands(self, email: str) -> tuple:
-        if not super().check_permissions(EnumRole.ADMIN, email):
-            return {"Error": "User doesn't have permissions"}, HTTPStatus.UNAUTHORIZED
+    def delete_commands(self, email: str, password: str) -> tuple:
+        super().check_permissions(EnumRole.ADMIN, email, password)
         self.commands.delete_many({})
         return '', HTTPStatus.NO_CONTENT
 
-    def get_all_commands(self, page: int, limit: int, email: str) -> tuple:
-        if not super().check_permissions(EnumRole.ADMIN, email):
-            return {"Error": "User doesn't have permissions"}, HTTPStatus.UNAUTHORIZED
+    def get_all_commands(self, page: int, limit: int, email: str, password: str) -> tuple:
+        super().check_permissions(EnumRole.ADMIN, email, password)
         to_skip = calc_to_skip(page, limit)
         return list(self.commands.find({}).sort("type", ASCENDING).skip(to_skip).limit(limit)), HTTPStatus.OK
