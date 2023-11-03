7de6944f611a19f9ead96cef80d3433cd716ee03
DeanAvram <dean6198@gmail.com>
2023-11-03 18:30:40 +0200

    Added password validation to objects service

--- src/controller/objects_controller.py
+++ src/controller/objects_controller.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from flask import request
 
 from src.services.rest.object_service import ObjectService
+from src.controller.main_controller import MainController
 
 objects = Blueprint('objects', __name__, url_prefix='/wastewise/objects')
 
@@ -11,19 +12,21 @@ objectService = ObjectService()
 @objects.post('')
 def create_object():
     data = request.get_json()
-    return objectService.create_object(_get_user_email(request), data)
+    return objectService.create_object(MainController.get_user_email(request),
+                                       MainController.get_user_password(request), data)
 
 
 @objects.get('/<object_id>')
 def get_object(object_id: str):
-    return objectService.get_object(_get_user_email(request), object_id)
-
+    return objectService.get_object(MainController.get_user_email(request),
+                                    MainController.get_user_password(request), object_id)
 
 
 @objects.put('/<object_id>')
 def update_object(object_id: str):
     data = request.get_json()
-    return objectService.update_object(_get_user_email(request), object_id, data)
+    return objectService.update_object(MainController.get_user_email(request),
+                                       MainController.get_user_password(request), object_id, data)
 
 
 def _get_user_email(req):
--- src/services/rest/object_service.py
+++ src/services/rest/object_service.py
@@ -1,5 +1,3 @@
-from jsonschema import ValidationError, validate
-
 from src.data.enum_role import EnumRole
 from src.services.rest.main_service import MainService
 from bson import json_util
@@ -14,21 +12,13 @@ class ObjectService(MainService):
         super().__init__()
         self.objects = super().get_db().objects
 
-    def create_object(self, mail: str, args: dict) -> tuple:
+    def create_object(self, email: str, password: str, args: dict) -> tuple:
 
-        if not super().check_permissions(EnumRole.USER, mail):
-            return {"Error": "User doesn't have permissions"}, HTTPStatus.UNAUTHORIZED
-
-        try:
-            validate(instance=args, schema=object_schema)
-        except ValidationError as e:
-            return {
-                "Error": str(e.schema["error_msg"] if "error_msg" in e.schema else e.message)}, HTTPStatus.BAD_REQUEST
-        except Exception as e:
-            return {"Error": str(e)}, HTTPStatus.BAD_REQUEST
+        super().check_permissions(EnumRole.USER, email, password)
+        MainService.validate_schema(args, object_schema)
 
         # create object
-        _object = Object(args['type'], mail)
+        _object = Object(args['type'], email)
 
         # check if args['data'] exists
         if 'data' in args:
@@ -40,31 +30,24 @@ class ObjectService(MainService):
         # return object as json
         return json.loads(_object.toJSON()), HTTPStatus.CREATED
 
-    def get_object(self, mail: str, object_id: str) -> tuple:
+    def get_object(self, email: str, password: str, object_id: str) -> tuple:
 
-        if not super().check_permissions(EnumRole.USER, mail):
-            return {"Error": "User doesn't have permissions"}, HTTPStatus.UNAUTHORIZED
+        super().check_permissions(EnumRole.USER, email, password)
 
         data = self.objects.find_one({'_id': object_id})
         if data is None:
             return {"Error": "Can't find object with id: " + object_id}, HTTPStatus.NOT_FOUND
         return json.loads(json_util.dumps(data)), HTTPStatus.OK
 
-    def update_object(self, mail: str, object_id: str, args: dict) -> tuple:
+    def update_object(self, email: str, password: str, object_id: str, args: dict) -> tuple:
         # FIXME: Working, but not good, the data is been overwritten
 
-        if not super().check_permissions(EnumRole.USER, mail):
-            return {"Error": "User doesn't have permissions"}, HTTPStatus.UNAUTHORIZED
-
-        try:
-            validate(instance=args, schema=object_schema_update)
-        except ValidationError as e:
-            return {
-                "Error": str(e.schema["error_msg"] if "error_msg" in e.schema else e.message)}, HTTPStatus.BAD_REQUEST
-        except Exception as e:
-            return {"Error": str(e)}, HTTPStatus.BAD_REQUEST
+        super().check_permissions(EnumRole.USER, email, password)
+        MainService.validate_schema(args, object_schema_update)
 
         _object = self.objects.find_one({'_id': object_id})  # get object from database
+        if _object is None:
+            return {"Error": "Can't find object with id: " + object_id}, HTTPStatus.NOT_FOUND
         _object = json.loads(json_util.dumps(_object))  # convert to json
 
         _object.update(args)  # update object with args
