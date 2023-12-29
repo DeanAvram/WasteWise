176a94026643fb3409abef117fd8ff5624060ec3
DeanAvram <dean6198@gmail.com>
2023-12-29 10:20:21 +0200

    Fix error message bug

--- src/services/rest/main_service.py
+++ src/services/rest/main_service.py
@@ -33,16 +33,17 @@ class MainService:
         try:
             validate(instance=args, schema=schema)
         except ValidationError as e:
-            path = e.path.pop()
-            if path == 'email':
-                abort(make_response(jsonify(message="Email is invalid"), HTTPStatus.BAD_REQUEST))
-            elif path == 'password':
-                abort(
-                    make_response(jsonify(message=str("Password is too short" if "short" in e.message else
-                                                      "Invalid password. Make sure it contains at least one digit, "
-                                                      "one uppercase letter, one lowercase letter and "
-                                                      "one special character")),
-                                  HTTPStatus.BAD_REQUEST))
+            if e.path.__len__() > 0:
+                path = e.path.pop()
+                if path == 'email':
+                    abort(make_response(jsonify(message="Email is invalid"), HTTPStatus.BAD_REQUEST))
+                elif path == 'password':
+                    abort(
+                        make_response(jsonify(message=str("Password is too short" if "short" in e.message else
+                                                          "Invalid password. Make sure it contains at least one digit, "
+                                                          "one uppercase letter, one lowercase letter and "
+                                                          "one special character")),
+                                      HTTPStatus.BAD_REQUEST))
             abort(make_response(jsonify(message=str(e.schema["error_msg"] if "error_msg" in e.schema else e.message)),
                                 HTTPStatus.BAD_REQUEST))
 
